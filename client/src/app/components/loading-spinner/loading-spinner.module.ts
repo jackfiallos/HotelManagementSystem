1748a0ea1f1ef1aba710a5556695a049a4626790
Jackfiallos <erling.fiallos@qbit.com.mx>
2017-10-30 15:07:26 +0000

    loading spinner as a module

---
+++ client/src/app/components/loading-spinner/loading-spinner.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { LoadingSpinnerComponent } from './loading-spinner.component';
+
+@NgModule({
+    imports: [],
+    declarations: [
+        LoadingSpinnerComponent
+    ],
+    exports: [
+        LoadingSpinnerComponent
+    ]
+})
+export class LoadingSpinnerModule {
+    constructor() {}
+}
